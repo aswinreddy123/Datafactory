9d115effb5a9cb8e5f3a4dff5c96e6983345d43b
Aswin Reddy <aswin.reddy@rabbitcircle.com>
2022-08-26 01:28:27 +0530

    new commit

---
+++ ADF.tf
@@ -0,0 +1,12 @@
+# Create Azure RG
+resource "azurerm_resource_group" "AZ-700" {
+  name     = "QuickCloudPOCsADF-RG"
+  location = "WestUS3"
+}
+
+# Create Azure Data factory
+resource "azurerm_data_factory" "QuickCloudPOCsADF" {
+  name                = "QuickCloudPOCsADF"
+  location            = azurerm_resource_group.QuickCloudPOCsADF.location
+  resource_group_name = azurerm_resource_group.QuickCloudPOCsADF.name
+}
---
+++ main.tf
@@ -0,0 +1,14 @@
+# ARM provider block -rekhu
+provider "azurerm" {
+  version = "~>2.0"
+  features {}
+}
+# Terraform backend configuration block -precreated
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "TerraformDemo"
+    storage_account_name = "terraformcodedemo"
+    container_name       = "tfstatefile"
+    key                  = "dev.terraform.tfstate"
+  }
+}
