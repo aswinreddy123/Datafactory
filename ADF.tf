95fea6ddb84e12fc834564a062e98d9926b7c881
Aswin Reddy <aswin.reddy@rabbitcircle.com>
2022-08-26 11:09:23 +0530

    yyyy

--- ADF.tf
+++ ADF.tf
@@ -10,3 +10,5 @@ resource "azurerm_data_factory" "QuickCloudPOCsADF" {
   location            = azurerm_resource_group.QuickCloudPOCsADF.location
   resource_group_name = azurerm_resource_group.QuickCloudPOCsADF.name
 }
+
+#test
